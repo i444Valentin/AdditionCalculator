c39b517446b5936f14feec11e2bdeb2ac7c6f3d7
Walentin <valession@gmail.com>
2021-03-11 14:48:37 +0300

    Add second empty activity Add jump to second activity

--- app/src/main/java/com/application/mathmachine/additioncalculator/MainActivity.java
+++ app/src/main/java/com/application/mathmachine/additioncalculator/MainActivity.java
@@ -2,13 +2,41 @@ package com.application.mathmachine.additioncalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button addButton;
+    Integer firstTerm;
+    Integer secondTerm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EditText firstTermEditText = findViewById(R.id.firstTerm);
+        EditText secondTermEditText = findViewById(R.id.secondTerm);
+
+        firstTerm = Integer.parseInt(firstTermEditText.getText().toString());
+        secondTerm = Integer.parseInt(secondTermEditText.getText().toString());
+        addButton = (Button) findViewById(R.id.addButton);
+
+        View.OnClickListener oAddButton = v -> {
+            Intent intentSecond = new Intent(this,SecondActivity.class);
+            startActivity(intentSecond);
+            intentSecond.putExtra("first_term",firstTerm);
+            intentSecond.putExtra("second_term",secondTerm);
+
+        };
+        addButton.setOnClickListener(oAddButton);
+    }
+
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
     }
 }
