c7647ed691911756e6650e3649c8c47490c49a2e
Walentin <valession@gmail.com>
2021-03-12 12:46:58 +0300

    Add addition of numbers Optimize project
    Add validation on a number

--- app/src/main/java/com/application/mathmachine/additioncalculator/MainActivity.java
+++ app/src/main/java/com/application/mathmachine/additioncalculator/MainActivity.java
@@ -1,18 +1,21 @@
 package com.application.mathmachine.additioncalculator;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 
 public class MainActivity extends AppCompatActivity {
-
     Button addButton;
     Integer firstTerm;
     Integer secondTerm;
+    TextView errorMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,23 +23,56 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         EditText firstTermEditText = findViewById(R.id.firstTerm);
         EditText secondTermEditText = findViewById(R.id.secondTerm);
+        errorMessage = findViewById(R.id.errorMessage);
 
-        firstTerm = Integer.parseInt(firstTermEditText.getText().toString());
-        secondTerm = Integer.parseInt(secondTermEditText.getText().toString());
+
         addButton = (Button) findViewById(R.id.addButton);
 
         View.OnClickListener oAddButton = v -> {
-            Intent intentSecond = new Intent(this,SecondActivity.class);
+            if (!isNumber(firstTermEditText, secondTermEditText)) {
+                errorMessage.setVisibility(View.VISIBLE);
+                errorMessage.setText("Вы можете ввести только целые числа. Проверьте ваши значения в полях.");
+                return;
+            }
+            errorMessage.setVisibility(View.INVISIBLE);
+            firstTerm = Integer.parseInt(firstTermEditText.getText().toString());
+            secondTerm = Integer.parseInt(secondTermEditText.getText().toString());
+            Intent intentSecond = new Intent(this, SecondActivity.class);
+            intentSecond.putExtra("first_term", firstTerm);
+            intentSecond.putExtra("second_term", secondTerm);
             startActivity(intentSecond);
-            intentSecond.putExtra("first_term",firstTerm);
-            intentSecond.putExtra("second_term",secondTerm);
 
         };
         addButton.setOnClickListener(oAddButton);
     }
 
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
+    /**
+     * Check is number
+     * This method checks two EditTexts (Inputs) on a number valid
+     *
+     * @param firstTerm  - first EditText
+     * @param secondTerm - second EditText
+     * @return - isNumber (true/false)
+     */
+    public boolean isNumber(EditText firstTerm, EditText secondTerm) {
+        return (firstTerm.getText().toString().matches("^-?([0-9]+)+$")
+                && secondTerm.getText().toString().matches("^-?([0-9]+)+$"));
+    }
 
+    /**
+     * Saves all values which writes in here
+     *
+     * @param outState current state
+     */
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("err_text", errorMessage.getText().toString());
+
+    }
+
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        String err_text = savedInstanceState.getString("err_text");
+        errorMessage.setText(err_text);
     }
 }
--- app/src/main/java/com/application/mathmachine/additioncalculator/SecondActivity.java
+++ app/src/main/java/com/application/mathmachine/additioncalculator/SecondActivity.java
@@ -1,14 +1,56 @@
 package com.application.mathmachine.additioncalculator;
 
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class SecondActivity extends AppCompatActivity {
+    TextView resultView;
+    Integer firstTerm;
+    Integer secondTerm;
+    String secondTermString;
+    String firstTermString;
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        resultView = findViewById(R.id.ResultText);
+        Bundle terms = getIntent().getExtras(); //init storage values
+        if (terms != null) {
+            firstTerm = terms.getInt("first_term"); //get first term
+            secondTerm = terms.getInt("second_term"); //get second term
+            String builtText = resultTextBuild();
+            printInTextView(builtText);
+        }
+
+    }
+
+    /**
+     * Builds result text for print in a TextView
+     *
+     * @return - built text
+     */
+    private String resultTextBuild() {
+        if (secondTerm < 0) {
+            secondTermString = "(" + secondTerm.toString() + ")";
+        } else {
+            secondTermString = secondTerm.toString();
+        }
+        firstTermString = firstTerm.toString();
+        return firstTermString + " + " + secondTermString + " = " + (firstTerm + secondTerm);
+    }
+
+    /**
+     * Prints text in a TextView
+     *
+     * @param text - string
+     */
+    private void printInTextView(String text) {
+
+        resultView.setText(text);
     }
 }
