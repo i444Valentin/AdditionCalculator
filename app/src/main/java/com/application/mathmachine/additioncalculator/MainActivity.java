3bf49f403c6e4543fa9f3a7754bdd10b0a0762eb
Walentin <valession@gmail.com>
2021-03-14 17:59:50 +0300

    Add exception catch

--- app/src/main/java/com/application/mathmachine/additioncalculator/MainActivity.java
+++ app/src/main/java/com/application/mathmachine/additioncalculator/MainActivity.java
@@ -16,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
     Integer firstTerm;
     Integer secondTerm;
     TextView errorMessage;
+    TextView exceptionMessage;
+    TextView exceptionText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,25 +26,33 @@ public class MainActivity extends AppCompatActivity {
         EditText firstTermEditText = findViewById(R.id.firstTerm);
         EditText secondTermEditText = findViewById(R.id.secondTerm);
         errorMessage = findViewById(R.id.errorMessage);
-
-
+        exceptionMessage = findViewById(R.id.exceptionMessage);
+        exceptionText = findViewById(R.id.exceptionText);
         addButton = (Button) findViewById(R.id.addButton);
+        try {
+            View.OnClickListener oAddButton = v -> {
+                if (!isNumber(firstTermEditText, secondTermEditText)) {
+                    errorMessage.setVisibility(View.VISIBLE);
+                    return;
+                }
+                errorMessage.setVisibility(View.INVISIBLE);
+                firstTerm = Integer.parseInt(firstTermEditText.getText().toString());
+                secondTerm = Integer.parseInt(secondTermEditText.getText().toString());
+                Intent intentSecond = new Intent(this, SecondActivity.class);
+                intentSecond.putExtra("first_term", firstTerm);
+                intentSecond.putExtra("second_term", secondTerm);
+                startActivity(intentSecond);
 
-        View.OnClickListener oAddButton = v -> {
-            if (!isNumber(firstTermEditText, secondTermEditText)) {
-                errorMessage.setVisibility(View.VISIBLE);
-                return;
-            }
-            errorMessage.setVisibility(View.INVISIBLE);
-            firstTerm = Integer.parseInt(firstTermEditText.getText().toString());
-            secondTerm = Integer.parseInt(secondTermEditText.getText().toString());
-            Intent intentSecond = new Intent(this, SecondActivity.class);
-            intentSecond.putExtra("first_term", firstTerm);
-            intentSecond.putExtra("second_term", secondTerm);
-            startActivity(intentSecond);
+            };
 
-        };
-        addButton.setOnClickListener(oAddButton);
+            addButton.setOnClickListener(oAddButton);
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            exceptionMessage.setVisibility(View.VISIBLE);
+            exceptionText.setVisibility(View.VISIBLE);
+            exceptionText.setText(exception.getMessage());
+        }
+
     }
 
     /**
@@ -66,6 +76,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putInt("err_msg_vis", errorMessage.getVisibility());
+        outState.putInt("exc_msg_vis", exceptionMessage.getVisibility());
+        outState.putInt("exc_text_vis", exceptionText.getVisibility());
+        outState.putString("exc_msg", exceptionText.getText().toString());
 
     }
 
@@ -76,7 +89,28 @@ public class MainActivity extends AppCompatActivity {
      */
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
-        int viewVisibility = savedInstanceState.getInt("err_msg_vis");
-        errorMessage.setVisibility(viewVisibility);
+        if (!savedInstanceState.isEmpty()) {
+            int viewVisibilityErrorMsg = savedInstanceState.getInt("err_msg_vis");
+            int viewVisibilityExceptionMsg = savedInstanceState.getInt("exc_msg_vis");
+            int viewVisibilityExceptionText = savedInstanceState.getInt("exc_text_vis");
+            String exceptionTextString = savedInstanceState.getString("exc_msg");
+            textViewUpdate(viewVisibilityErrorMsg, viewVisibilityExceptionMsg, viewVisibilityExceptionText, exceptionTextString);
+        }
+
+    }
+
+    /**
+     * This method updates these TextViews
+     *
+     * @param viewVisibilityErrorMsg      -
+     * @param viewVisibilityExceptionMsg  -
+     * @param viewVisibilityExceptionText - key of TextView visibility
+     * @param exceptionTextString         - Exception TextView text
+     */
+    private void textViewUpdate(int viewVisibilityErrorMsg, int viewVisibilityExceptionMsg, int viewVisibilityExceptionText, String exceptionTextString) {
+        errorMessage.setVisibility(viewVisibilityErrorMsg);
+        exceptionMessage.setVisibility(viewVisibilityExceptionMsg);
+        exceptionText.setVisibility(viewVisibilityExceptionText);
+        exceptionText.setText(exceptionTextString);
     }
 }
